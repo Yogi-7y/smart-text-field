96b9401f1c06eff01ef98d391a8a07018634d289
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2024-12-17 10:36:40 +0530

    feat(searchable_dropdown): add debounce support

--- lib/src/module/searchable_dropdown_field/presentation/state/search_results_notifier.dart
+++ lib/src/module/searchable_dropdown_field/presentation/state/search_results_notifier.dart
@@ -4,21 +4,64 @@ import 'package:flutter/material.dart';
 
 import '../../../../../smart_textfield.dart';
 
+typedef Widgets = List<Widget>;
+
 class SearchResultsNotifier extends ChangeNotifier {
   SearchResultsNotifier({
     required this.sources,
+    this.debounceDuration = _defaultDebounceDuration,
   });
+
+  static const _defaultDebounceDuration = Duration(milliseconds: 300);
 
   final SearchSources sources;
 
+  final Duration debounceDuration;
+  Timer? _debounceTimer;
+
   final _widgets = <Widget>[];
   List<Widget> get widgets => _widgets;
+
+  @override
+  void dispose() {
+    _debounceTimer?.cancel();
+    super.dispose();
+  }
 
   Future<void> search(
     BuildContext context,
     Query query,
   ) async {
+    _debounceTimer?.cancel();
+
+    _debounceTimer = Timer(
+      debounceDuration,
+      () async => _onDebounceCallback(context: context, query: query),
+    );
+  }
+
+  Future<void> _onDebounceCallback({
+    required BuildContext context,
+    required Query query,
+  }) async {
     _widgets.clear();
+
+    final _results = await searchQuery(
+      context: context,
+      query: query,
+    );
+
+    _widgets.addAll(_results);
+
+    notifyListeners();
+  }
+
+  @visibleForTesting
+  Future<Widgets> searchQuery({
+    required BuildContext context,
+    required Query query,
+  }) async {
+    final _results = <Widget>[];
 
     for (final source in sources) {
       final _provider = source.provider;
@@ -30,7 +73,7 @@ class SearchResultsNotifier extends ChangeNotifier {
             .map((item) => _renderer.render(context, item)) //
             .toList();
 
-        _widgets.addAll(_result);
+        _results.addAll(_result);
       } else if (_provider is AsyncSearchProvider) {
         final _providerResults = await _provider.search(query);
 
@@ -38,10 +81,10 @@ class SearchResultsNotifier extends ChangeNotifier {
             .map((item) => _renderer.render(context, item)) //
             .toList();
 
-        _widgets.addAll(_result);
+        _results.addAll(_result);
       }
     }
 
-    notifyListeners();
+    return _results;
   }
 }
